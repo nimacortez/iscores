63c7f805c74563b9990767f1b9e74153f52f36e3
Nima H <nimahz@Nimas-MacBook-Air.local>
2023-05-23 19:08:54 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+const options = {
+  method: 'GET',
+  url: 'https://free-nba.p.rapidapi.com/games',
+  params: {
+    page: '0',
+    per_page: '25'
+  },
+  headers: {
+    'X-RapidAPI-Key': '4d73882bf5msh8772805fa93dc92p13371djsnf79699fec785',
+    'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+  }
+};
 
 function App() {
+  const [Scores, setScores] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.request(options);
+        console.log(response.data)
+        setScores(response.data); // Update the state with the response data
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Render your Scores state here */}
       </header>
     </div>
   );
